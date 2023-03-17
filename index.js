5bc9c8bb2afb99b8664588e45ec850ebe7de9433
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-03-17 14:07:38 +0000

    add & link orders file

--- index.js
+++ index.js
@@ -22,3 +22,18 @@ themeToggler.addEventListener('click', () => {
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
     
 })
+
+// fill orders in table
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                <td>${order.productName}</td>
+                <td>${order.productNumber}</td>
+                <td>${order.paymentStatus}</td>
+                <td class="${order.shipping === 'Cancelled' ? 'danger' : order.shipping === 'pending' ? 'warning' : 'primary'}" >${order.shipping}</td>
+                <td class="primary">Details</td>  
+    `;
+
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Apple Macbook M1 Pro 14inch',
+        productNumber: '98765',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Dell Inspiron 7570',
+        productNumber: '34567',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'Keychron K1 Mechanical Keyboard',
+        productNumber: '83946',
+        paymentStatus: 'Refunded',
+        shipping: 'Cancelled',
+    },
+    {
+        productName: 'iPhone 14 Pro Max 256GB',
+        productNumber: '37829',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'LG 34" UltraWide Curved Monitor',
+        productNumber: '20458',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+]
